25ac86b1e6d175a608e222c9acf3fe64b7de603b
Wojciech <wojciechonoszko@gmail.com>
2022-06-20 11:30:51 +0200

    Update Filter.js

--- src/components/filter/Filter.js
+++ src/components/filter/Filter.js
@@ -3,25 +3,44 @@ import PropTypes from 'prop-types';
 import {InputContainer, LabelContainer} from '../contactForm/ContactFormStyles';
 
 
-const Filter = ({handleChange, filter}) => {
-
+export default function Filter({ data }) {
     return(
         <>
             <LabelContainer htmlFor="filter">Find contacts by name</LabelContainer>
             <InputContainer
                 id="filter"
                 type="text"
-                onChange={handleChange}
+                onChange={data}
                 name="filter"
-                value={filter}
+                
             />
         </>
     );
-};
+}
 
 Filter.propTypes = {
-    handleChange: PropTypes.func.isRequired,
-    filter: PropTypes.string.isRequired
+    data: PropTypes.func.isRequired,
 };
 
-export default Filter;
+// const Filter = ({handleChange, filter}) => {
+
+//     return(
+//         <>
+//             <LabelContainer htmlFor="filter">Find contacts by name</LabelContainer>
+//             <InputContainer
+//                 id="filter"
+//                 type="text"
+//                 onChange={handleChange}
+//                 name="filter"
+//                 value={filter}
+//             />
+//         </>
+//     );
+// };
+
+// Filter.propTypes = {
+//     handleChange: PropTypes.func.isRequired,
+//     filter: PropTypes.string.isRequired
+// };
+
+// export default Filter;
